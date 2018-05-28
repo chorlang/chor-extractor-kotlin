c9a8ab7e70e451f79be2c8ab596891ecad18710b
fmontesi <famontesi@gmail.com>
2018-05-28 15:17:02 +0200

    Hello

--- pe/src/jmh/kotlin/bench/BenchTest.kt
+++ pe/src/jmh/kotlin/bench/BenchTest.kt
@@ -11,10 +11,10 @@ import org.openjdk.jmh.runner.RunnerException
 
 
 
-@Warmup(iterations = 10)
-@Measurement(iterations = 10)
-@BenchmarkMode(Mode.SingleShotTime)
-@OutputTimeUnit(TimeUnit.SECONDS)
+@Warmup(iterations = 3)
+@Measurement(iterations = 3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 
 
 open class BenchTest{
@@ -46,5 +46,98 @@ open class BenchTest{
     fun tst(){
         val args = arrayOf("-g", "50", "10")
         NetworkProjection.main(args)
+    }
+
+    @Benchmark
+    fun sanitaryAgency(){
+        val test =
+                "citizen{" +
+                        "def X{" +
+                        "sanagency!<request>; sanagency?; sanagency!<provInf>; sanagency&{" +
+                        "refusal: X, " +
+                        "acceptance: coop?; bank!<paymentPrivateFee>; X}} " +
+                        "main{X}" +
+                        "} | " +
+                        "sanagency{" +
+                        "def X{" +
+                        "citizen?; citizen!<askInfo>; citizen?; if infoProved " +
+                        "then citizen+acceptance; coop!<req>; bank!<paymentPublicFee>; bank?; X " +
+                        "else citizen+refusal; X }" +
+                        "main {X}} | " +
+                        "coop{def X{" +
+                        "sanagency?; " +
+                        "if fine " +
+                        "then citizen!<provT>; bank+recMoneyPossT; bank?; X " +
+                        "else citizen!<provM>; bank+recMoneyPossM; bank?; X} " +
+                        "main{X}} | " +
+                        "bank{" +
+                        "def X{ coop&{" +
+                        "recMoneyPossT: coop!<paymentT>; Y, " +
+                        "recMoneyPossM: coop!<paymentM>; Y}} " +
+                        "def Y{citizen?; sanagency?; sanagency!<done>; X} " +
+                        "main{X}}"
+
+        val args = arrayOf("-c", test, "-l", "coop, bank")
+
+        val actual = ChoreographyExtraction.main(args)
+    }
+
+    @Benchmark
+    fun sanitaryAgency2x(){
+        val test =
+                "citizen{" +
+                        "def X{" +
+                        "sanagency!<request>; sanagency?; sanagency!<provInf>; sanagency&{" +
+                        "refusal: X, " +
+                        "acceptance: coop?; bank!<paymentPrivateFee>; X}} " +
+                        "main{X}" +
+                        "} | " +
+                        "sanagency{" +
+                        "def X{" +
+                        "citizen?; citizen!<askInfo>; citizen?; if infoProved " +
+                        "then citizen+acceptance; coop!<req>; bank!<paymentPublicFee>; bank?; X " +
+                        "else citizen+refusal; X }" +
+                        "main {X}} | " +
+                        "coop{def X{" +
+                        "sanagency?; " +
+                        "if fine " +
+                        "then citizen!<provT>; bank+recMoneyPossT; bank?; X " +
+                        "else citizen!<provM>; bank+recMoneyPossM; bank?; X} " +
+                        "main{X}} | " +
+                        "bank{" +
+                        "def X{ coop&{" +
+                        "recMoneyPossT: coop!<paymentT>; Y, " +
+                        "recMoneyPossM: coop!<paymentM>; Y}} " +
+                        "def Y{citizen?; sanagency?; sanagency!<done>; X} " +
+                        "main{X}}" +
+                        " | citizen2{" +
+                        "def X{" +
+                        "sanagency2!<request>; sanagency2?; sanagency2!<provInf>; sanagency2&{" +
+                        "refusal: X, " +
+                        "acceptance: coop2?; bank2!<paymentPrivateFee>; X}} " +
+                        "main{X}" +
+                        "} | " +
+                        "sanagency2{" +
+                        "def X{" +
+                        "citizen2?; citizen2!<askInfo>; citizen2?; if infoProved " +
+                        "then citizen2+acceptance; coop2!<req>; bank2!<paymentPublicFee>; bank2?; X " +
+                        "else citizen2+refusal; X }" +
+                        "main {X}} | " +
+                        "coop2{def X{" +
+                        "sanagency2?; " +
+                        "if fine " +
+                        "then citizen2!<provT>; bank2+recMoneyPossT; bank2?; X " +
+                        "else citizen2!<provM>; bank2+recMoneyPossM; bank2?; X} " +
+                        "main{X}} | " +
+                        "bank2{" +
+                        "def X{ coop2&{" +
+                        "recMoneyPossT: coop2!<paymentT>; Y, " +
+                        "recMoneyPossM: coop2!<paymentM>; Y}} " +
+                        "def Y{citizen2?; sanagency2?; sanagency2!<done>; X} " +
+                        "main{X}}"
+
+        val args = arrayOf("-c", test, "-l", "coop, bank, coop2, bank2")
+
+        val actual = ChoreographyExtraction.main(args)
     }
 }
