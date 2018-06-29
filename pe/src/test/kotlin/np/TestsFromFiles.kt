32b79bc51589026824080d932d6b38aaa4266b1f
lsafina <safinalrr@gmail.com>
2018-06-29 14:55:05 +0300

    testing tests

---
+++ pe/src/test/kotlin/np/TestsFromFiles.kt
@@ -0,0 +1,54 @@
+package np
+
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class TestsFromFiles {
+    @Test
+    fun test() {
+        //finding tests
+        val dirPath = "tests"
+        val dir = File(dirPath)
+        require(dir.exists() && dir.isDirectory() && dir.list().isNotEmpty())
+
+        //configuration crap
+        val tests = HashMap<String, String>()
+        val testName = "([*]{3}) C([0-9]+) ([*]{3})".toRegex()
+        val emptyString =  "^\$".toRegex()
+        var title = ""
+        var text = ""
+
+        //collecting choreographies from test files
+        for (fileName in dir.list()){
+            val file = File("$dirPath/$fileName")
+            file.forEachLine {
+                when {
+                    it.matches(testName) -> {
+                        title = it.removeSurrounding("***").removeSurrounding(" ")
+                    }
+                    it.matches(emptyString) -> {
+                        tests[title] = text
+                        title = ""
+                        text = ""
+                    }
+                    else -> text+=it
+                }
+            }
+        }
+
+        //process choreographies
+        tests.forEach { title, test ->
+            println("TEST: $title")
+            println("CHOREOGRAPHY: $test")
+
+            try {
+                val result = NetworkProjection.project(test).toString()
+                println("NETWORK: $result")
+                println()
+            } catch (e: MergingProjection.MergingException){
+                println("Tets failed with exception")
+            }
+
+        }
+    }
+}
