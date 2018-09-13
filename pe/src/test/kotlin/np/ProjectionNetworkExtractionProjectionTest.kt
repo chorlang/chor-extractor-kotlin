22ffd57a58aef94fdbef48dee50189f769bb9170
lsafina <safinalrr@gmail.com>
2018-09-13 14:05:50 +0300

    renaming

--- pe/src/test/kotlin/np/ProjectionNetworkExtractionProjectionTest.kt
+++ pe/src/test/kotlin/np/ProjectionNetworkExtractionProjectionTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 
-class ProjectionExtractionProjectionTest {
+class ProjectionNetworkExtractionProjectionTest {
     @Test
     fun tst1() {
         val test = "def X {" +
