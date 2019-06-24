22be7020cc34365cb01a34759951da2208bebc43
lsafina <safinalrr@gmail.com>
2019-06-24 11:27:04 +0200

    test for the chapter

---
+++ pe/src/test/kotlin/epp/ThesisTest.kt
@@ -0,0 +1,42 @@
+package epp
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ThesisTest {
+
+    @Test
+    fun tcpTest() {
+        val test = "def X {Y} def Y { p.e->q; stop } main {q.e->p;X}"
+
+        val tcp = "def TCP {" +
+                        "client.syn -> server; if server.createSocketSuccess " +
+                            "then server -> client[synRcv]; server.syn -> client; server.ack -> client; client.ack -> server; Established " +
+                            "else server -> client[rst]; stop } " +
+                  "def Established { " +
+                        "if client.transmissionInProgress " +
+                            "then client -> server[transmission]; Established " +
+                            "else client -> server[fin]; server.fin -> client; server.ack -> client; client.ack -> server; stop } " +
+                  "main {TCP}"
+
+
+        val actual = EndPointProjection.project(tcp).toString()
+        //  println(EndPointProjection.getChoreographyStatistic(f))
+        val expected =
+                "server{" +
+                        "def TCP{client?; if createSocketSuccess " +
+                            "then client + synRcv; client!<syn>; client!<ack>; client?; Established " +
+                            "else client + rst; stop} " +
+                        "def Established{client&{transmission: Established, fin: client!<fin>; client!<ack>; client?; stop}} " +
+                        "main {TCP}} " +
+                "| client{" +
+                        "def TCP{server!<syn>; server&{rst: stop, synRcv: server?; server?; server!<ack>; Established}} " +
+                        "def Established{if transmissionInProgress " +
+                            "then server + transmission; Established " +
+                            "else server + fin; server?; server?; server!<ack>; stop} " +
+                        "main {TCP}}"
+
+        assertEquals(expected, actual)
+    }
+}
